847078bb5b2d201095279a7959cbc81f264234ef
Thassya <thayowisky@gmail.com>
2020-04-30 21:57:13 +0200

    Ajustando HTTPClient

--- src/app/contacts/contact.service.ts
+++ src/app/contacts/contact.service.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Contact } from './contact';
-import { Http, Response } from '@angular/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class ContactService {
     private contactsUrl = '/api/contacts';
 
-    constructor (private http: Http) {}
+    constructor (private http: HttpClient) {}
 
     // get("/api/contacts")
     getContacts(): Promise<void | Contact[]> {
       return this.http.get(this.contactsUrl)
                  .toPromise()
-                 .then(response => response.json() as Contact[])
+                 .then(response => response as Contact[])
                  .catch(this.handleError);
     }
 
@@ -20,7 +20,7 @@ export class ContactService {
     createContact(newContact: Contact): Promise<void | Contact> {
       return this.http.post(this.contactsUrl, newContact)
                  .toPromise()
-                 .then(response => response.json() as Contact)
+                 .then(response => response as Contact)
                  .catch(this.handleError);
     }
 
@@ -30,7 +30,7 @@ export class ContactService {
     deleteContact(delContactId: String): Promise<void | String> {
       return this.http.delete(this.contactsUrl + '/' + delContactId)
                  .toPromise()
-                 .then(response => response.json() as String)
+                 .then(response => response as String)
                  .catch(this.handleError);
     }
 
@@ -39,7 +39,7 @@ export class ContactService {
       var putUrl = this.contactsUrl + '/' + putContact._id;
       return this.http.put(putUrl, putContact)
                  .toPromise()
-                 .then(response => response.json() as Contact)
+                 .then(response => response as Contact)
                  .catch(this.handleError);
     }
 
